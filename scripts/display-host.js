31a815ae0614a7ac560d425a9808d3c1d9bf1f89
Alexey <alexeyorc@gmail.com>
2018-11-22 16:05:23 +0200

    Updated display-host.js

--- scripts/display-host.js
+++ scripts/display-host.js
@@ -1,7 +1,9 @@
 chrome.storage.sync.get(['host'], function (result) {
-    if (result.host !== undefined) {
+    if (result.host !== '') {
         document.getElementById('host').value = result.host;
         updateIconHref(result.host);
+    } else {
+        updateIconHref('https://www.oxygenxml.com/xml_web_author.html');
     }
 });
 
